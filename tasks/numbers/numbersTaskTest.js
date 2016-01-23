23a30775e43f0925eb45e554515d6c845bb4668c
xarvh <francesco.orsenigo@gmail.com>
2016-01-24 09:26:47 +1100

    Add count test

--- tasks/numbers/numbersTaskTest.js
+++ tasks/numbers/numbersTaskTest.js
@@ -20,14 +20,14 @@ lab.experiment('Numbers Task', () => {
     lab.after((done) => { delete global._; done(); });
 
 
-
     lab.test('accepts an initial inverse speed', (done) => {
 
         var tellNumberTimestamps = [];
+        var onCount = sinon.spy();
 
         var task = new NumbersTask({
             formatTimestamp: (a) => a,
-            onCount: sinon.spy(),
+            onCount: onCount,
             tellNumber: () => {
                 tellNumberTimestamps.push(new Date);
                 if (tellNumberTimestamps.length > 4) {
@@ -54,7 +54,72 @@ lab.experiment('Numbers Task', () => {
                 return current;
             });
 
+            // 5 numbers are called, but the first one is not counted, and the
+            // game is terminated before the last one can register as 'miss'.
+            assert.deepEqual(onCount.args, [ ['miss', 25], ['miss', 25], ['miss', 25] ]);
+
+            // test aggregate data
+            var aggregate = task.getAggregateEvents();
+            assert.equal(aggregate.length, 2)
+
+            var session = _.zipObject(aggregate[0], aggregate[1]);
+            assert.equal(session['Right'], 0);
+            assert.equal(session['Wrong'], 0);
+            assert.equal(session['Miss'], 3);
+            assert.equal(session['Accuracy (normalized)'], 0);
+            assert.equal(session['Starting inverse speed'], 25);
+            assert.equal(session['Max inverse speed'], 25);
+            assert.equal(session['Min inverse speed'], 25);
+            assert.equal(session['Trials at min inverse speed'], 3);
+
+            // test events
+            assert.deepEqual(_.map(task.getEvents(), 2), ['Event', 'start', 'miss', 'miss', 'miss', 'stop']);
+
             done();
         });
     });
+
+
+    lab.test('correctly counts trials', (done) => {
+
+        var onCount = sinon.spy();
+
+        var task;
+        var numbers = []
+        var onNumber = (n) => setImmediate(function () {
+            switch (numbers.length) {
+                case 0: break;
+                case 1: task.setUserAnswer(20); break; // wrong
+                case 2: break; // miss
+                case 3: task.setUserAnswer(_.last(numbers) + n); break; // right
+                case 4: task.stop(); break;
+            }
+
+            numbers.push(n);
+        });
+
+        task = new NumbersTask({
+            formatTimestamp: (a) => a,
+            onCount: onCount,
+            tellNumber: onNumber,
+        });
+
+        task.start(25, 25);
+        timeoutSet(25 * 6, function () {
+            assert.deepEqual(onCount.args, [ ['wrong', 25], ['miss', 25], ['right', 25] ]);
+
+            var aggregate = task.getAggregateEvents();
+            var session = _.zipObject(aggregate[0], aggregate[1]);
+            assert.equal(session['Right'], 1);
+            assert.equal(session['Wrong'], 1);
+            assert.equal(session['Miss'], 1);
+            assert.equal(session['Accuracy (normalized)'], 1/3);
+
+            assert.deepEqual(_.map(task.getEvents(), 2), ['Event', 'start', 'wrong', 'miss', 'right', 'stop']);
+            done();
+        });
+    });
+
+
+    lab.test('adjusts the difficulty to the user performance');
 });
