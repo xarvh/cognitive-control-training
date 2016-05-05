e7a7961927346784ef3eeb53184503a4670f26a5
xarvh <francesco.orsenigo@gmail.com>
2016-05-05 21:22:56 +1000

    Pasat buttons show outcome on click

--- src/PasatView.elm
+++ src/PasatView.elm
@@ -18,8 +18,6 @@ buttonRadius =
 
 
 -- TODO move this out of here
-
-
 voices =
   [ "english/ossi"
   , "italiano/fra"
@@ -38,11 +36,11 @@ languageOptions currentVoice =
     List.map voiceToOption voices
 
 
-makeButton : Int -> Float -> Float -> Html Pasat.Action
-makeButton answer radius angle =
+makeButton : Int -> Bool -> Float -> Float -> Html Pasat.Action
+makeButton answer isCorrect radius angle =
   div
     [ onClick <| Pasat.NestedPstAction <| PacedSerialTask.UserAnswers answer
-    , class "number-button"
+    , class <| "number-button number-button-" ++ if isCorrect then "right" else "wrong"
     , style
         [ ( "top", toString (buttonContainerHeight / 2 - buttonRadius / 2 - 1.1 * radius * buttonRadius * cos (turns angle)) ++ "px" )
         , ( "left", toString (buttonContainerHeight / 2 - buttonRadius / 2 + 1.1 * radius * buttonRadius * sin (turns angle)) ++ "px" )
@@ -55,10 +53,10 @@ makeButton answer radius angle =
 -- Lay out the more frequent answers closer to the centre
 
 
-makeButtons : List (Html Pasat.Action)
-makeButtons =
+makeButtons : Maybe Pasat.Answer -> List (Html Pasat.Action)
+makeButtons correctAnswer =
   let
-    bt = makeButton
+    bt n = makeButton n (Just n == correctAnswer)
   in
     -- centre
     [ bt 10 0 0
@@ -146,7 +144,7 @@ view model =
             [ style [ ( "display", "inline-block" ) ] ]
             [ div
                 [ class "number-buttons-container" ]
-                (makeButtons)
+                (makeButtons <| model.pst.key model.pst.sessionPqs)
             , let
                 ( action, label ) =
                   if model.pst.isRunning then
